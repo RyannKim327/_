205aca933af26e93ff57b976f3e096f7788aa31a
RyannKim327 <rksesgundo123@gmail.com>
2025-06-21 16:14:18 +0800

    06-21-25 16:14 - @RyannKim327

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/_/",
+  base: "/Chinatgpt/",
   plugins: [react(), tailwindcss()],
 });
