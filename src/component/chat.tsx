28b840fef94ad3b52fb710055d1fbc197d07cd8d
RyannKim327 <rksesgundo123@gmail.com>
2025-06-30 05:37:00 +0800

    06-30-25  5:37 - @RyannKim327 [Added PWA]

--- src/component/chat.tsx
+++ src/component/chat.tsx
@@ -4,18 +4,18 @@ import type { Message } from "../types";
 export default function Chat(message: Message) {
   return (
     <div
-      className={`flex ${message.role === "user" ? "justify-end" : "justify-start"} w-full`}
+      className={`flex ${message.role === "user" ? "justify-end" : "justify-start"} w-full md:px-4`}
     >
       <div
         className={
           message.role === "user"
-            ? `bg-slate-700 p-1 px-2 rounded max-w-3/4`
-            : `p-1 px-2 max-w-full`
+            ? `bg-slate-700 p-1 px-2 rounded max-w-3/`
+            : `p-1 px-2 text-justify`
         }
       >
         <p
           dangerouslySetInnerHTML={{
-            __html: marked(message.content),
+            __html: marked(message.content).toString(),
           }}
         ></p>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/_/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicons.svg", "robots.txt"],
+      manifest: {
+        name: "ChinatGPT",
+        short_name: "GPT",
+        description: "My simple ChatGPT UI",
+        theme_color: "#333333",
+        icons: [
+          {
+            src: "./src/assets/react.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
 });
